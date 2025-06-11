e2c732669bc076424155769407af6784ddfaf130
ghadiyaharsh <harshghadiya212@gmail.com>
2025-06-11 22:24:49 +0530

    update

--- Visitor_Counter/Visitor_Counter/urls.py
+++ Visitor_Counter/Visitor_Counter/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Visitor_Counter import views 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('visitor/', views.visitor ,name='visitor')
 ]
--- Visitor_Counter/Visitor_Counter/views.py
+++ Visitor_Counter/Visitor_Counter/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
 
-
+def visitor(request):
+    info={
+        'name':['raj','jay','shiv'] 
+        
+        
+        }
+    
+    return render(request, "index.html",info)
     
