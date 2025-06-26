cd567f44d1e349758352b58ea5d87cc3bca70651
ghadiyaharsh <harshghadiya212@gmail.com>
2025-06-26 22:54:40 +0530

    Initial commit

---
+++ Image_Upload_and_Preview/Image_Upload_and_Preview/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Image_Upload_and_Preview project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Image_Upload_and_Preview.settings')
+
+application = get_asgi_application()
